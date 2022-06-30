5bd96da61dbf1808f6bcb4c73d88b7c2a0c2defb
Mamadaminovich <mamadaminov001@gmail.com>
2022-06-30 17:36:14 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+ $(".single-item").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    speed: 500,
+    Infinity: true,
+    arrows: true,
+    prevArrow:
+      '<button id="prevButton"><img src="img/Forma 1 copy 8.png"></button>',
+    nextArrow: '<button id="nextButton"><img src="img/Forma 1.png"></button>',
+  });
+  $(".blog_slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    speed: 500,
+    Infinity: true,
+    arrows: true,
+    prevArrow:
+      '<button id="prevButton_2"><img src="img/Forma 1 copy 8.png"></button>',
+    nextArrow: '<button id="nextButton_2"><img src="img/Forma 1.png"></button>',
+  });
